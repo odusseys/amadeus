a48e18f2d2108bd8beb27256b63143121a9aaab7
Ulysse Mizrahi <ulysse@fitle.com>
2018-03-20 23:52:19 +0200

    style

--- src/App.js
+++ src/App.js
@@ -4,21 +4,61 @@ import store, { persistor } from 'store';
 import logo from './logo.svg';
 import './App.css';
 import { PersistGate } from 'redux-persist/integration/react';
+import styled from 'styled-components';
+import colors from 'style/colors';
+
+const Page = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  color: ${colors.black};
+`;
+
+const Body = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+const LeftNav = styled.div`
+  width: 180px;
+  height: 100%;
+  background: ${colors.coal};
+  color: white;
+  padding: 10px;
+`;
+
+const MainContainer = styled.div`
+  flex: 1;
+  background-color: ${colors.lightGrey};
+  overflow: auto;
+  display: flex;
+`;
+
+const Main = styled.div`
+  margin: 10px 10px 0 10px;
+  background-color: white;
+  width: 100%;
+`;
+
+const Title = styled.h1``;
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <PersistGate persistor={persistor} loading={null}>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-          </div>
+          <Page>
+            <Body>
+              <LeftNav>
+                <Title>AMADEUS</Title>
+              </LeftNav>
+              <MainContainer>
+                <Main>main</Main>
+              </MainContainer>
+            </Body>
+          </Page>
         </PersistGate>
       </Provider>
     );
---
+++ src/style/colors.js
@@ -0,0 +1,8 @@
+export default {
+  red: 'red',
+  white: 'white',
+  lightGrey: '#f5f5f5',
+  grey: 'grey',
+  black: 'black',
+  coal: '#3c3c3c'
+};
